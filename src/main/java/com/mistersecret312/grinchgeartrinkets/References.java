a0628f4b92075e507edc635afdce3c09f665ce5f
mistersecret312 <sergiigamer7@gmail.com>
2025-08-30 18:30:20 +0300

    Reference fix.

--- src/main/java/com/mistersecret312/grinchgeartrinkets/References.java
+++ src/main/java/com/mistersecret312/grinchgeartrinkets/References.java
@@ -2,11 +2,11 @@ package com.mistersecret312.grinchgeartrinkets;
 
 public class References
 {
-    public static final String MOD_ID = "baublescom";
+    public static final String MOD_ID = "grinchgeartrinkets";
     public static final String VERSION = "1.0";
     public static final String MC_VERSION = "[1.12.2]";
-    public static final String NAME = "Baubles Commission";
+    public static final String NAME = "Grinch's Gear and Trinkets";
 
-    public static final String CLIENT_PROXY_CLASS = "com.mistersecret312.baublescom.ClientProxy";
-    public static final String COMMON_PROXY_CLASS = "com.mistersecret312.baublescom.CommonProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.mistersecret312.grinchgeartrinkets.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "com.mistersecret312.grinchgeartrinkets.CommonProxy";
 }
